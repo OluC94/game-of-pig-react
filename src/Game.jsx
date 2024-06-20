634d40ef4eb3c17f73d94d00382717c4b2c8249b
OluC94 <coker.olu94@gmail.com>
2024-06-20 15:01:00 +0100

    progress through challenge

---
+++ src/Game.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react"
+
+export default function Game() {
+    const [player1Score, setPlayer1Score] = useState(0)
+    const [player2Score, setPlayer2Score] = useState(0)
+    const [playerTurn, setPlayerTurn] = useState("Player 1");
+    const [isGameInProgress, setIsGameInProgress] = useState(true);
+    const [lastRoll, setLastRoll] = useState(0);
+    const [turnTotal, setTurnTotal] = useState(0);
+    const [target, setTarget] = useState(2);
+    // const [winnerMessage, setWinnerMessage] = useState("")
+
+    function handleRoll(){
+        const roll = Math.floor(Math.random() * 6) + 1;
+        setLastRoll(roll);
+
+        roll === 1 ? changeTurn() : setTurnTotal(currTotal => currTotal + roll);
+    }
+
+    function handleStick(){
+        if (playerTurn === "Player 1") {
+            setPlayer1Score(currScore => currScore += turnTotal)
+        }
+        if (playerTurn === "Player 2") {
+            setPlayer2Score(currScore => currScore += turnTotal)
+        }
+        changeTurn();
+
+    }
+
+    function changeTurn(){
+        handleEndGame();
+        if (playerTurn === "Player 1") {
+            setPlayerTurn("Player 2")
+            setTurnTotal(0)
+        }
+        if (playerTurn === "Player 2") {
+            setPlayerTurn("Player 1")
+            setTurnTotal(0)
+        }
+    }
+
+    function handleNewGame(gameType) {
+        setPlayer1Score(0)
+        setPlayer2Score(0)
+        setLastRoll(0)
+        setTurnTotal(0)
+        setIsGameInProgress(true);
+        setPlayerTurn("Player 1")
+        gameType === "long" ? setTarget(100) : setTarget(30)
+    }
+
+    function handleEndGame() {
+        if (player1Score >= target || player2Score >= target) {
+            console.log("game won")
+            setIsGameInProgress(false);
+        }
+    }
+
+    return (<div>
+        <h1>
+            Game Of Pig
+        </h1>
+        <h3>First to {target}</h3>
+
+        <div>
+            <p>P1 score: {player1Score}</p>
+            <p>P2 score: {player2Score}</p>
+        </div>
+
+        <div>
+            <button onClick={handleRoll}>
+                Roll
+            </button>
+            {isGameInProgress ?  <p>{playerTurn}'s turn</p> : <p>{playerTurn} wins</p>}
+            <p>Last Roll: {lastRoll}</p>
+            <p>Turn total: {turnTotal}</p>
+
+            <button onClick={handleStick}>
+                Stick
+            </button>
+        </div>
+
+        <div>
+            <button onClick={() => handleNewGame("long")}>
+            New Game
+            </button>
+
+            <button onClick={() => handleNewGame("short")}>
+            New Short Game
+            </button>
+        </div>
+
+
+    </div>)
+}
